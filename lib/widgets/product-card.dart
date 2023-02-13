29963d84b523058e42ad39beda3da34817b99756
Mauricio Alpizar <ealpizar@ticosoftweb.com>
2023-02-12 18:43:39 -0600

    Elaborando las tarjetas con imagenes

--- lib/widgets/product-card.dart
+++ lib/widgets/product-card.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 class ProductCard extends StatelessWidget {
-  
   //
   //
   const ProductCard({super.key});
@@ -16,15 +14,17 @@ class ProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
-        margin: const EdgeInsets.only(top: 15 , bottom: 50),
+        margin: const EdgeInsets.only(top: 15, bottom: 50),
         width: double.infinity,
         height: 400,
         //color: Colors.red,
         decoration: _boxDecorationRadius(),
+        child: Stack(
+          children: const [_backGroundImage()],
+        ),
       ),
     );
   }
-
 
   //
   //
@@ -35,15 +35,30 @@ class ProductCard extends StatelessWidget {
     return BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(25),
-        boxShadow:const [
+        boxShadow: const [
           BoxShadow(
             color: Colors.black12,
-            offset: Offset(0,8),
+            offset: Offset(0, 8),
             blurRadius: 10,
-        )
+          )
+        ]);
+  }
+}
 
-        ] 
+class _backGroundImage extends StatelessWidget {
+  const _backGroundImage({
+    Key? key,
+  }) : super(key: key);
 
-      );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 400,
+      child: const FadeInImage(
+        fit: BoxFit.contain,
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('http://www.mauricioalpizar.com/wp-content/uploads/2023/02/FlutterImage-1024x576.jpeg')),
+    );
   }
 }
